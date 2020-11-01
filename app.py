a71939c1c9628785bfc36245673152a08a45e620
Andrew Burns <burnsad@tcd.ie>
2020-11-01 13:52:40 +0000

    add task form

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 
@@ -93,6 +93,10 @@ def logout():
     flash("You have been logged out of your Fine Gael Account")
     session.pop("user")
     return redirect(url_for("login"))
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    return render_template("add_task.html")
 
 
 if __name__ == "__main__":
